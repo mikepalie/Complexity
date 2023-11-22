127e1873ee9bc8b68e258f357a5ce620638a1c7f
mikepalie <michalis.palierakis@gmail.com>
2023-11-22 19:33:16 +0200

    buble

--- Complexity/Program.cs
+++ Complexity/Program.cs
@@ -6,12 +6,12 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            int[] mixdata = { 56, 23, 2, 86, 45, 102 };
+            int[] mixdata = { 56, 23, 2, 86, 102, 45 };
             int temp;
 
-            for (int j = 0; j <= mixdata.Length - 2; j++)
+            for (int j = 0; j < mixdata.Length - 1; j++)
             {
-                for (int i = 0; i <= mixdata.Length - 2; i++)
+                for (int i = 0; i < mixdata.Length - 1; i++)
                 {
                     if (mixdata[i] > mixdata[i + 1])
                     {
